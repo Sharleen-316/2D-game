49df3130aca8a130cff959cd03329bae63b32896
Sharleen Hossain <shos0038@student.monash.edu>
2025-12-16 19:04:09 +1100

    AI enemy that follows player around

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    // # Reference to transform of player
     Transform player;
+
+    // # Speed of rotation
+    public float rotSpeed = 180f;
+
+
     // Update is called once per frame
     void Update()
     {
@@ -25,6 +31,13 @@ public class EnemyMovement : MonoBehaviour
         }
 
         // # Turn to face player if its has been found
-        Vector3 dir = player.position - transform.position; // # Points to player
+        Vector3 dir = player.position - transform.position; // # Gets the distance between player and enemy
+        dir.Normalize(); // # Ensure the distance equals 1
+
+        float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90; // # Gets the angle the enemy have in order to face the player
+
+        Quaternion desiredRot = Quaternion.Euler(0, 0, zAngle); // # Sets the new rotation
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRot, rotSpeed * Time.deltaTime); // # Sets the speed of the rotation
     }
 }
